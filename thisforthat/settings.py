fa58b5df911a7011e413fd17e89b408f476ac50d
Richard Sipinski <root@ip-10-112-31-101.(none)>
2013-02-06 04:58:58 +0000

    changed db settings

--- thisforthat/settings.py
+++ thisforthat/settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'thisforthat',
         'PASSWORD': 'thisforthat',
-        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': 'devdb.hackshack.ca',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
 }
