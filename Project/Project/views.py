060afb035a1f4f9d54aeaca349bf14ca2d3a748e
sipinski <Richard.Sipinski@ad.umanitoba.ca>
2013-02-22 00:27:49 -0600

    new branch. Testing the django restful framework

--- Project/Project/views.py
+++ Project/Project/views.py
@@ -4,6 +4,9 @@ from django.template.loader import get_template
 from TFT.models import User
 #from django.contrib.auth.models import User
 from TFT.models import Country
+
+from rest_framework import Request
+
 import datetime
 
 def home(request):
