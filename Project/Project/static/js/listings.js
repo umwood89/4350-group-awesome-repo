b9f69aa65cd6d8c8b7703eacff3e16b31bd1cf7a
sipinski <Richard.Sipinski@ad.umanitoba.ca>
2013-02-25 01:48:20 -0600

    add listings.js

---
+++ Project/Project/static/js/listings.js
@@ -0,0 +1,46 @@
+// load the following using JQuery's document ready function
+$(function(){
+
+    // Listing model
+    var Listing = Backbone.Model.extend({});
+
+    // set up the view for a Listing
+    var ListingView = Backbone.View.extend({
+        render: function () {
+            // template with ICanHaz.js (ich)
+            this.el = ich.ListingRowTpl(this.model.toJSON());
+            return this;
+        }
+    });
+
+    // define the collection of Listings
+    var ListingCollection = Backbone.Collection.extend({
+        model: Listing,
+        url: '/api/listings/'
+    });
+
+    // main app
+    var AppView = Backbone.View.extend({
+        tagName: 'tbody',
+
+        initialize: function() {
+            // instantiate a Listing collection
+            this.Listings = new ListingCollection();
+            this.Listings.bind('all', this.render, this);
+            this.Listings.fetch();
+        },
+
+        render: function () {
+            // template with ICanHaz.js (ich)
+            this.Listings.each(function (Listing) {
+                $(this.el).append(new ListingView({model: Listing}).render().el);
+            }, this);
+
+            return this;
+        }
+    });
+
+    var app = new AppView();
+    $('#app').append(app.render().el);
+});
+
