d851584a56a5276dcff30b99b809c2d693790478
Walter Fotiuk <wfotiuk@gmail.com>
2013-02-25 01:00:53 -0600

    Got a table of all Offers to display ont he main page
    Duplicate sitself

---
+++ Project/Project/static/js/offers.js
@@ -0,0 +1,40 @@
+
+$(function(){
+    var Offer = Backbone.Model.extend({});
+    // set up the view for a Offer
+    var OfferView = Backbone.View.extend({
+        render: function () {
+            // template with ICanHaz.js (ich)
+            this.el = ich.OfferRowTpl(this.model.toJSON());
+            return this;
+        }
+    });
+    // define the collection of Offers
+    var OfferCollection = Backbone.Collection.extend({
+        model: Offer,
+        url: '/api/offers/'
+    });
+    // main app
+    var AppView = Backbone.View.extend({
+        tagName: 'tbody',
+        initialize: function() {
+            // instantiate a Offer collection
+            this.Offers = new OfferCollection();
+            this.Offers.bind('all', this.render, this);
+            this.Offers.fetch();
+        },
+ 
+        render: function () {
+            // template with ICanHaz.js (ich)
+            this.Offers.each(function (Offer) {
+                $(this.el).append(new OfferView({model: Offer}).render().el);
+            }, this);
+ 
+            return this;
+        }
+    });
+ 
+    var app = new AppView();
+    $('#app').append(app.render().el);
+});
+
