499130b9f5de137eaa589842958553dce772b9eb
Walter Fotiuk <wfotiuk@gmail.com>
2013-02-24 21:43:26 -0600

    ADded backbone dependencies

--- Project/TFT/models.py
+++ Project/TFT/models.py
@@ -28,3 +28,16 @@ class Offer(models.Model):
 	date_accepted = models.DateTimeField()
 	def __unicode__(self):
 		return self.title
+
+class Password(models.Model):
+    title = models.CharField(max_length=200)
+    username = models.CharField(max_length=200, blank=True)
+    password = models.CharField(max_length=200)
+    url = models.URLField(max_length=500,blank=True,verbose_name='Site URL')
+    notes = models.TextField(max_length=500,blank=True,help_text='Any extra notes')
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    def __unicode__(self):
+        return self.title
+
+
