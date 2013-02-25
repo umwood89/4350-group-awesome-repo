f429b848da1db144e5df3d9466d07c4be7a38ff4
Jordan Wood <jordanmyleswood@gmail.com>
2013-02-24 21:51:47 -0600

    Listings from database are shown in the carousel
    - the title and description from the latest 10 listings will show up in
    the carousel
    - images are currently not showing

--- Project/Project/views.py
+++ Project/Project/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.template import Template, Context, RequestContext
 from django.template.loader import get_template
+from TFT.models import Listing
 import datetime
 
 def home(request):
-    t = get_template('index.html')
-    html = t.render(Context())
-    return HttpResponse(html)
+	listings = Listing.objects.order_by('date_created')[:10]
+	t = get_template('index.html')
+	c = Context({"listings": listings})
+	html = t.render(c)
+	return HttpResponse(html)
