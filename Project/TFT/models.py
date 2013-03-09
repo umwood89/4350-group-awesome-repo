b260c1b91698abb7e747e6cbaef756356e0c8283
sipinski <Richard.Sipinski@ad.umanitoba.ca>
2013-03-09 00:18:50 -0600

    fix models.py

--- Project/TFT/models.py
+++ Project/TFT/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
@@ -29,35 +28,3 @@ class Offer(models.Model):
 	date_accepted = models.DateTimeField()
 	def __unicode__(self):
 		return self.title
-=======
-from django.db import models
-from django.contrib.auth.models import User
-import datetime
-
-# Create your models here.
-
-class Listing(models.Model):
-	listing_id = models.AutoField(primary_key=True)
-	title = models.CharField(max_length=255)
-	description = models.TextField(max_length=500)
-	user = models.ForeignKey(User)
-	photo = models.ImageField(upload_to='static/media/listing_photos/')
-	trade_completed = models.BooleanField()
-	date_created = models.DateTimeField(auto_now_add=True)
-	date_completed = models.DateTimeField(blank = True, null = True)
-	def __unicode__(self):
-		return self.listing_id
-	
-class Offer(models.Model):
-	offer_id = models.AutoField(primary_key=True)
-	listing = models.ForeignKey('Listing')
-	title = models.CharField(max_length=255)
-	description = models.TextField(max_length=500)
-	user = models.ForeignKey(User)
-	photo = models.ImageField(upload_to='static/media/offer_photos/')
-	offer_accepted = models.BooleanField()
-	date_created = models.DateTimeField(auto_now_add=True)
-	date_accepted = models.DateTimeField(blank = True, null = True)
-	def __unicode__(self):
-		return self.offer_id
->>>>>>> Gave up trying backbone, using built-in django stuff. Pretty successful
