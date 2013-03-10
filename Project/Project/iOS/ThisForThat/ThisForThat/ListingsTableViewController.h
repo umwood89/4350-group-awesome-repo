4870499710b7561f5107235a4eca206c71d426b6
kyleshewchuk <umshewck@cc.umanitoba.ca>
2013-03-10 14:18:12 -0500

    Removed default tab bar controller pages and added custom Listings and Offers views. Also added a navigation bar on the top of each page.

---
+++ Project/Project/iOS/ThisForThat/ThisForThat/ListingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListingsTableViewController.h
+//  ThisForThat
+//
+//  Created by Kyle Shewchuk on 2013-03-10.
+//  Copyright (c) 2013 Kyle Shewchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListingsTableViewController : UITableViewController
+
+@end
---
+++ Project/Project/iOS/ThisForThat/ThisForThat/OffersTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  OffersTableViewController.h
+//  ThisForThat
+//
+//  Created by Kyle Shewchuk on 2013-03-10.
+//  Copyright (c) 2013 Kyle Shewchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OffersTableViewController : UITableViewController
+
+@end
