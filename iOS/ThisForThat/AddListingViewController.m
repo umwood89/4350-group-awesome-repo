d8b9a642061eb6450706617ea2c3e30d69ec520d
Richard Sipinski <Richard.Sipinski@ad.umanitoba.ca>
2013-03-17 19:57:13 -0500

    Add listing now posting json to server

--- iOS/ThisForThat/AddListingViewController.m
+++ iOS/ThisForThat/AddListingViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "AddListingViewController.h"
+#import "ASIHTTPRequest.h"
 
 @interface AddListingViewController ()
 
@@ -36,9 +37,28 @@
 }
 
 - (IBAction)createListing:(id)sender {
+    NSString *title = self.addListingTitleTextBox.text;
+    NSString *description = self.addListingDescriptionTextBox.text;
+    NSInteger user = 3;
+    NSString *photo = @"photopath";
     
+    NSURL *url = [NSURL URLWithString:@"http://hackshack.ca/api/listings/"];
+    ASIHTTPRequest *request = [ASIHTTPRequest requestWithURL:url];
     
+    [request addRequestHeader:@"Accept" value:@"application/json"];
+    [request addRequestHeader:@"Content-Type" value:@"application/json; encoding=utf-8"];
     
+    //NSString *dataContent = [NSString stringWithFormat:@"username=%@&password=%@", username, password];
+    
+    NSString *dataContent = [NSString stringWithFormat:@"{\"title\": \"%@\", \"description\": \"%@\", \"user\": %d, \"photo\": \"%@\" }", title, description, user, photo];
+    
+    NSLog(@"dataContent: %@", dataContent);
+    [request appendPostData:[dataContent dataUsingEncoding:NSUTF8StringEncoding]];
+    [request setRequestMethod:@"POST"];
+    [request startSynchronous];
+    NSString *response = [request responseString];
+    NSLog(@"JSONAddListing response: %@", response);
+
     
 }
 - (IBAction)cancelButton:(id)sender {
