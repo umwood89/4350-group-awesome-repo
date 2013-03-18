4b27d2382c85bcc6f0b16a9b6a0dbad1eab13cfa
Walter Fotiuk <wfotiuk@gmail.com>
2013-03-18 00:18:44 -0500

    updated tests

--- iOS/ThisForThat/ThisForThatTests/ThisForThatTests.m
+++ iOS/ThisForThat/ThisForThatTests/ThisForThatTests.m
@@ -72,13 +72,12 @@
     //assert(o.date_accepted);
 }
 
-/*-(void)testGetUsers
+-(void)testGetUsers
 {
     UserData *u = [JSONInterface.users objectAtIndex:0];
     assert(u);
     //assert(u.id);
     assert(u.username);
-    assert(u.password);
     assert(u.email);
 
 }
@@ -86,16 +85,16 @@
 -(void) testAddListing
 {
     int origCount = JSONInterface.listings.count;
-    ListingData *l = [[ListingData alloc] initWithData:@"Tester" description:@"This is a test" photo:@"listing_photos/test.jpg" trade_completed:0 date_created: @"" date_completed: @""];
+    ListingData *l = [[ListingData alloc] initWithData:@"1" title:@"Tester" description:@"This is a test" photo:@"listing_photos/test.jpg" trade_completed:0 date_created: @"" date_completed: @""];
     
-    JSONInterface.addListing(l);
+    [JSONInterface addListing:l ];
     assert(JSONInterface.listings.count == origCount +1);
     
     ListingData *l2 = [JSONInterface.listings objectAtIndex:origCount];
     
     assert(l);
     assert(l2);
-    assert(l.id == l2.id);
+    assert(l.lid == l2.lid);
     assert(l.title == l2.title);
     assert(l.description == l2.description);
     assert(l.photo == l2.photo);
@@ -107,16 +106,16 @@
 -(void) testAddOffer
 {
     int origCount = JSONInterface.offers.count;
-    OfferData *o = [[OfferData alloc] initWithData:@"Tester" description:@"This is a test" photo:@"listing_photos/test.jpg" offer_accepted: 0 date_created: @"" date_accepted: @""];
+    OfferData *o = [[OfferData alloc] initWithData:@"1" title:@"Tester" description:@"This is a test" photo:@"listing_photos/test.jpg" offer_accepted: 0 date_created: @"" date_accepted: @""];
     
-    JSONInterface.addOffer(o);
+    [JSONInterface addOffer:o];
     assert(JSONInterface.offers.count == origCount +1);
     
     OfferData *o2 = [JSONInterface.offers objectAtIndex:origCount];
     
     assert(o);
     assert(o2);
-    assert(o.id == o2.id);
+    assert(o.oid == o2.oid);
     assert(o.title == o2.title);
     assert(o.description == o2.description);
     assert(o.photo == o2.photo);
@@ -128,19 +127,19 @@
 -(void) testAddUser
 {
     int origCount = JSONInterface.users.count;
-    UserData *u = [[UserData alloc] initWithData:@"test user" email:"test@test.com" ];
+    UserData *u = [[UserData alloc] initWithData:@"1" username:@"test user" email:@"test@test.com" ];
     
-    JSONInterface.addUser(l);
+    [JSONInterface addUser:u];
     assert(JSONInterface.users.count == origCount +1);
     
-    UserData *u2 = JSONInterface.users.objectAtIndex:count;
+    UserData *u2 = [JSONInterface.users objectAtIndex:origCount];
     
     assert(u);
     assert(u2);
-    assert(u.id == u2.id);
+    assert(u.uid == u2.uid);
     assert(u.username = u2.username);
     assert(u.email = u2.email);
 
-}*/
+}
 
 @end
