989cdc04817861f7f97769a4430085ffa61cd265
Walter Fotiuk <wfotiuk@gmail.com>
2013-03-17 23:26:30 -0500

    Added the Userdata class and wrote some JSON stuff for it

---
+++ iOS/ThisForThat/ThisForThat/UserData.h
@@ -0,0 +1,13 @@
+//
+//  UserData.h
+//  ThisForThat
+//
+//  Created by Walter Fotiuk on 3/17/13.
+//  Copyright (c) 2013 Kyle Shewchuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserData : NSObject
+
+@end
---
+++ iOS/ThisForThat/ThisForThat/UserData.m
@@ -0,0 +1,13 @@
+//
+//  UserData.m
+//  ThisForThat
+//
+//  Created by Walter Fotiuk on 3/17/13.
+//  Copyright (c) 2013 Kyle Shewchuk. All rights reserved.
+//
+
+#import "UserData.h"
+
+@implementation UserData
+
+@end
