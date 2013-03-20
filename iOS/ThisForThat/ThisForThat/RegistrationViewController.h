b99ad4220359da835496a1233148312fb36d0ba2
Richard Sipinski <Richard.Sipinski@ad.umanitoba.ca>
2013-03-20 00:13:37 -0500

    Added registration view

---
+++ iOS/ThisForThat/ThisForThat/RegistrationViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegistrationViewController.h
+//  ThisForThat
+//
+//  Created by Richard Sipinski on 2013-03-19.
+//  Copyright (c) 2013 Kyle Shewchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegistrationViewController : UIViewController
+
+@end
---
+++ iOS/ThisForThat/ThisForThat/RegistrationViewController.m
@@ -0,0 +1,38 @@
+//
+//  RegistrationViewController.m
+//  ThisForThat
+//
+//  Created by Richard Sipinski on 2013-03-19.
+//  Copyright (c) 2013 Kyle Shewchuk. All rights reserved.
+//
+
+#import "RegistrationViewController.h"
+
+@interface RegistrationViewController ()
+
+@end
+
+@implementation RegistrationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
