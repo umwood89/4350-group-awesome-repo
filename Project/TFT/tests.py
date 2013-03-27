3ab1a31cc12cb4242c529695bb246372cc9c8638
sipinski <Richard.Sipinski@ad.umanitoba.ca>
2013-03-26 22:56:59 -0500

    tests

--- Project/TFT/tests.py
+++ Project/TFT/tests.py
@@ -5,8 +5,9 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 
 
-THIS FOR THAT MAIN TESTS FILE. 
+Some simple Django tests 
 
+These are in addition to the built-in django tests, and the selenium tests. 
 """
 from django.utils import unittest
 from django.test import TestCase
@@ -16,19 +17,17 @@ from django.contrib.auth.models import User
 
 import datetime
 
+
+
 class ListingTestCase(unittest.TestCase):
         def setUp(self):
-            self.listing = Listing(description="description", title="title", photo="photo.jpg", trade_completed=0, user=User(), date_completed=datetime.datetime.now()) 
+            self.listing = Listing(description="description", title="title", photo="photo.jpg", trade_completed=0, user=User(), date_completed=datetime.datetime.now())    
             
         def test_listing_model_data(self):
             self.assertEqual(self.listing.title, "title")
             self.assertEqual(self.listing.photo, "photo.jpg")
             self.assertEqual(self.listing.trade_completed, 0)
             
-            
-            
-
-
 class OfferTestCase(unittest.TestCase):
         def setUp(self):
             self.offer = Offer(title="title", description="description", offer_id=1, listing=Listing(), user=User(), photo="photo.jpg", offer_accepted=0, date_accepted=datetime.datetime.now())
@@ -37,8 +36,6 @@ class OfferTestCase(unittest.TestCase):
             """ Test the functionalty of the listings model """
             self.assertEqual(self.offer.title, "title")
             self.assertEqual(self.offer.description, "description")
-            
-        
 
 
 class ProfileTestCase(unittest.TestCase):
@@ -52,6 +49,14 @@ class ProfileTestCase(unittest.TestCase):
             self.assertEqual(self.profile.url, "url")
             self.assertEqual(self.profile.company, "company")
             self.assertEqual(self.profile.location, "location")
+
+class UserTestCase(unittest.TestCase):
+    def setUp(self):
+        self.user = User(username="richard")
+    
+    def test_delete_user(self):
+        self.user.delete()
+        self.assertEqual(self.user, null)
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
