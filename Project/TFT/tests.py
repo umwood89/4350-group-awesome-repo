bd5f47ccc1dff1103ca490219da450a9707c9753
sipinski <Richard.Sipinski@ad.umanitoba.ca>
2013-03-26 22:42:04 -0500

    More django testing

--- Project/TFT/tests.py
+++ Project/TFT/tests.py
@@ -18,32 +18,33 @@ import datetime
 
 class ListingTestCase(unittest.TestCase):
         def setUp(self):
-            self.listing = Listing(description="description", title="title", listing_id=34, photo="photo.jpg", trade_completed=0, user=0, date_completed=datetime.now()) 
+            self.listing = Listing(description="description", title="title", photo="photo.jpg", trade_completed=0, user=User(), date_completed=datetime.datetime.now()) 
             
         def test_listing_model_data(self):
             self.assertEqual(self.listing.title, "title")
-            self.assertEqual(self.listing.listing_id,34)
             self.assertEqual(self.listing.photo, "photo.jpg")
             self.assertEqual(self.listing.trade_completed, 0)
-            self.assertEqual(self.listing.user, 0)
+            
+            
+            
 
 
 class OfferTestCase(unittest.TestCase):
         def setUp(self):
-            self.offer = Offer(title="title", description="description", offer_id=1, listing=1, user=0, photo="photo.jpg", offer_accepted=0, date_accepted=datetime.now())
+            self.offer = Offer(title="title", description="description", offer_id=1, listing=Listing(), user=User(), photo="photo.jpg", offer_accepted=0, date_accepted=datetime.datetime.now())
 
         def test_offer_model_data(self):
             """ Test the functionalty of the listings model """
             self.assertEqual(self.offer.title, "title")
             self.assertEqual(self.offer.description, "description")
-            self.assertEqual(self.offer.user,0)
+            
         
 
 
 class ProfileTestCase(unittest.TestCase):
         def setUp(self):
             self.user = User()
-            self.profile = UserProfile(user,userphoto="photo.jpg",url="url",company="company",location="location")
+            self.profile = UserProfile(user=User(),userphoto="photo.jpg",url="url",company="company",location="location")
 
         def test_profile_model_data(self):
             """ Test the functionality of the offer model """
